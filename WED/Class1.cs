aa70f009d7f0bf7b8ff09e8b70f5d27b7153c883
Chloe3c <kristianishenko21@gmail.com>
2024-12-25 19:43:04 +0200

    Добавьте файлы проекта.

---
+++ WED/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Coin
+{
+    private int nominal;
+    private int quantity;
+
+    public Coin(int nominal, int quantity)
+    {
+        this.nominal = nominal;
+        this.quantity = quantity;
+    }
+
+    public string GetInformation()
+    {
+        return $"Номинал: {nominal}, Количество: {quantity}, Ответ: {CalculateTotal()}";
+    }
+
+    public int CalculateTotal()
+    {
+        return nominal * quantity;
+    }
+}
---
+++ WED/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите номинал монеты (1, 2, 5 или 10): ");
+        int nominal = int.Parse(Console.ReadLine());
+
+        if (nominal != 1 && nominal != 2 && nominal != 5 && nominal != 10)
+        {
+            Console.WriteLine("Номинал монеты должен быть 1, 2, 5 или 10.");
+            return;
+        }
+
+        Console.Write("Введите количество монет: ");
+        int quantity = int.Parse(Console.ReadLine());
+
+        Coin coin = new Coin(nominal, quantity);
+
+        Console.WriteLine(coin.GetInformation());
+    }
+}
